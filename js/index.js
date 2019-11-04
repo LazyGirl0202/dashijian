8fe4dcd42dc3bf7115f4aa49c1cca8215fe07791
徐上 <1243349930@qq.com>
2019-11-04 15:13:10 +0800

    徐上第二次上传

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 $(function(){
       // 获取最新评论信息
+      var myDate = new Date()
+      var month=myDate.getMonth()+1;
+      console.log(month);
+      
       $.ajax({
         type: 'get',
         url: 'http://47.111.184.55:8888/api/v1/index/latest_comment',
         success: function (data) {
+          // console.log(data);
+          data.month=month;
           console.log(data);
+          
           var html = template('comment_listBoxTpl', data)
           // console.log(html);
   
