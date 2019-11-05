04d7c2a1e03579886e7efafa04aa91bb9a787e1f
LazyGirl <1424217526@qq.com>
2019-11-05 10:54:38 +0800

    文章类别第三次修改

--- admin/js/category.js
+++ admin/js/category.js
@@ -29,6 +29,7 @@ $(function () {
   //3.根据id查询指定文章类别
   // 点击编辑按钮
   $('#tplBox').on('click', '.modify', function () {
+    //获取id
     var id = $(this).attr('data-id')
     $.ajax({
       url: 'http://47.111.184.55:8888/api/v1/admin/category/search',
@@ -36,32 +37,32 @@ $(function () {
         id: id
       },
       success: function (data) {
-        console.log(data);
-        console.log(data.data[0]);
-
+        //console.log(data);
+        //console.log(data.data[0]);
         var html = template('modifyModalTpl', data.data[0]);
         $('#addModalBox').html(html);
       }
     })
+
+    //让弹框显示
     $('#addModal').modal('show');
   })
-
+  //点击退出让弹框消失
   $('#addModal').on('click', '#model_shutoend', function () {
     $('#addModal').modal('hide');
+    location.reload();
   });
-
 
   // 模态框提交表单
   $('#addModal').on('submit', '#modifyModal', function () {
     var formdata = $(this).serialize()
     console.log(formdata);
-
     $.ajax({
       url: 'http://47.111.184.55:8888/api/v1/admin/category/edit',
       type: 'post',
       data: formdata,
       success: function (data) {
-        console.log(data);
+        //console.log(data);
         location.reload();
       },
       error: function (data) {
@@ -72,14 +73,13 @@ $(function () {
   })
 
 
-
   //5.删除文章类别
   $('#tplBox').on('click', '.delete', function () {
     var id = $(this).attr('data-id')
     console.log(id)
     if (confirm("您确定要删除吗？")) {
       $.ajax({
-        url: `http://47.111.184.55:8888/api/v1/admin/category/delete`,
+        url: 'http://47.111.184.55:8888/api/v1/admin/category/delete',
         type: 'post',
         data: { id },
         success: function (data) {
